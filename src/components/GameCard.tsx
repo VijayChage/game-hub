9343366f3fd3e54dbbc395f034e32aa2315e2ada
Vijay Chage <vijay.chage@gmail.com>
2024-08-19 06:21:34 +0530

    Added croppedImages function

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,7 @@
 import { Card, CardBody, Heading, Image, Text } from "@chakra-ui/react";
 import { Game } from "../hooks/useGames";
 import PlatformIconList from "./PlatformIconList";
+import getCroppedImageUrl from "../services/image-url";
 
 interface Props {
   game: Game;
@@ -8,8 +9,8 @@ interface Props {
 
 const GameCard = ({ game }: Props) => {
   return (
-    <Card borderRadius={10} overflow="hidden">
-      <Image src={game.background_image}></Image>
+    <Card width="300px" borderRadius={10} overflow="hidden">
+      <Image src={getCroppedImageUrl(game.background_image)}></Image>
       <CardBody>
         <Heading fontSize="2xl">{game.name}</Heading>
         <PlatformIconList
