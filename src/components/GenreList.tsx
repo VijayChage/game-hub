12f5b3b7d6fe0ce1739918fcdcebc18f4d3f48bb
Vijay Chage <vijay.chage@gmail.com>
2024-08-26 12:44:03 +0530

    Highligthing selected Genre

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -7,20 +7,25 @@ import {
   ListItem,
   Spinner,
 } from "@chakra-ui/react";
-import useGenres from "../hooks/useGenres";
+import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
 
-const GenreList = () => {
+interface Props {
+  onSelectGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null;
+}
+
+const GenreList = ({ selectedGenre, onSelectGenre }: Props) => {
   const { data, isLoading } = useGenres();
   if (isLoading) return <Spinner />;
   return (
     <>
       <Heading fontSize="2xl" marginTop={9} marginBottom={3}>
-        Genres
+        {}
       </Heading>
       <List>
         {data.map((genre) => (
-          <ListItem key="genre.id">
+          <ListItem key={genre.id} padding="5px">
             <HStack>
               <Image
                 boxSize="32px"
@@ -29,13 +34,12 @@ const GenreList = () => {
                 src={getCroppedImageUrl(genre.image_background)}
               ></Image>
               <Button
-                // whiteSpace="normal"
-                // textAlign="left"
-                // fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
-                // onClick={() => onSelectGenre(genre)}
+                whiteSpace="normal"
+                textAlign="left"
+                fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
                 fontSize="lg"
                 variant="link"
-                onClick={() => console.log(genre)}
+                onClick={() => onSelectGenre(genre)}
               >
                 {genre.name}
               </Button>
--- src/hooks/useData.ts
+++ src/hooks/useData.ts
@@ -1,12 +1,16 @@
 import { useEffect, useState } from "react";
 import apiClient from "../services/api-client";
-import { CanceledError } from "axios";
+import { AxiosRequestConfig, CanceledError } from "axios";
 
 interface FetchResponse<T> {
   count: number;
   results: T[];
 }
-const useData = <T>(endpoint: string) => {
+const useData = <T>(
+  endpoint: string,
+  requestConfig?: AxiosRequestConfig,
+  deps?: any
+) => {
   const [data, setData] = useState<T[]>([]);
   const [error, setError] = useState("");
   const [isLoading, setIsLoading] = useState(false);
@@ -15,18 +19,15 @@ const useData = <T>(endpoint: string) => {
     const controller = new AbortController();
     setIsLoading(true);
     apiClient
-      .get<FetchResponse<T>>(endpoint, { signal: controller.signal })
+      .get<FetchResponse<T>>(endpoint, {
+        signal: controller.signal,
+        ...requestConfig,
+      })
       .then((res) => {
-        // console.log(
-        //   "Inside useData Sucess: res.data.results",
-        //   res.data.results
-        // );
         setData(res.data.results);
         setIsLoading(false);
       })
       .catch((err) => {
-        // console.log("Inside useData Error: err.message", err.message);
-
         if (err instanceof CanceledError) return;
         {
           setError(err.message);
